2140b2503efc4fbe67b237b815b6b241d2f1dbee
Saanvi-cool <84757010+Saanvi-cool@users.noreply.github.com>
2021-07-09 13:40:49 -0400

    Add files via upload

---
+++ awesome.js
@@ -0,0 +1,5 @@
+function login(){
+    username=document.getElementById("user_name").value;
+    localStorage.setItem("username_key",username);
+    window.location="kwitter_room.html";
+}
---
+++ kwitter.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCAheWkrjZxapEMljpHXr9ohfQce8sZXKc",
+    authDomain: "twitter-8e621.firebaseapp.com",
+    projectId: "twitter-8e621",
+    storageBucket: "twitter-8e621.appspot.com",
+    messagingSenderId: "563818798460",
+    appId: "1:563818798460:web:8a211c444a1c48f85a58f9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
